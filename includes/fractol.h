3f092e029539c5997bf47872ab14137e58754008
lopoka <lopoka@student.hive.fi>
2024-05-31 13:00:52 +0300

    Introduced t_fract struct

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,12 +6,30 @@
 /*   By: lopoka <lopoka@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 15:06:19 by lopoka            #+#    #+#             */
-/*   Updated: 2024/05/29 15:27:13 by lopoka           ###   ########.fr       */
+/*   Updated: 2024/05/31 12:45:34 by lopoka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+#ifndef FRACTOL_H
+# define FRACTOL_H
+# include "MLX42.h"
+# define WIDTH	1000
+# define HEIGHT	1000
 typedef struct s_complex
 {
 	double	r;
 	double	i;
 }	t_complex;
+
+typedef struct s_fract
+{
+	mlx_t		mlx;
+	mlx_image_t	img;
+	int			precision;
+	double		min_x;
+	double		max_x;
+	double		min_y;
+	double		max_y;
+	double		shift_x;
+	double		shift_y;
+	double		zoom;
+}	t_fract
--- sources/fractol.c
+++ sources/fractol.c
@@ -6,11 +6,12 @@
 /*   By: lopoka <lopoka@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/30 17:47:44 by lopoka            #+#    #+#             */
-/*   Updated: 2024/05/30 17:48:28 by lopoka           ###   ########.fr       */
+/*   Updated: 2024/05/31 13:00:09 by lopoka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../MLX42/include/MLX42/MLX42.h"
+#include "../includes/fractol.h"
 #include <stdlib.h>
 
 typedef struct s_complex
@@ -37,12 +38,23 @@ t_complex ft_complex_square(t_complex n)
 	return (res);
 }
 
-double map(double unscaled_num, double new_min, double new_max, double old_min, double old_max)
+
+
+double ft_map(double val, t_fract *stc, int axis)
+{
+	if (!axis)
+    	return (val * (stc->max_y - stc->min_y) / HEIGTH + stc->min_y);
+	else	
+    	return (val * (stc->max_x - stc->min_x) / WIDTH + stc->min_x);
+}
+
+
+double ft_map2(double unscaled_num, double new_min, double new_max, double old_min, double old_max)
 {
     return (new_max - new_min) * (unscaled_num - old_min) / (old_max - old_min) + new_min;
 }
 
-void	ft_in_mandelbrot(mlx_image_t *img, int row, int col)
+void	ft_in_mandelbrot(t_fract *stc, int row, int col)
 {
 	t_complex	z;
 	t_complex	c;
@@ -50,8 +62,8 @@ void	ft_in_mandelbrot(mlx_image_t *img, int row, int col)
 
 	z.r = 0;
 	z.i = 0;
-	c.r = map(row, -2, 2, 0, 800);
-	c.i = map(col, 2, -2, 0, 800);
+	c.r = ft_map(row, stc, 0);
+	c.i = ft_map(col, stc, 1);
 
 	for (int i = 0; i < 100; i++)
 	{
@@ -59,38 +71,39 @@ void	ft_in_mandelbrot(mlx_image_t *img, int row, int col)
 		if ((z.r * z.r + z.i * z.i) > 4)
 		{
 			color = map(i, 0x000000, 0xFFFFFF, 0, 100); // black white
-			mlx_put_pixel(img, row, col, color);
+			mlx_put_pixel(stc->img, row, col, color);
 			return ;
 		}
 	}
-	mlx_put_pixel(img, row, col, 0x660066); //Purple
+	mlx_put_pixel(stc->img, row, col, 0x660066); //Purple
 }
 
 int main(void)
 {
+	t_fract		stc;
 	mlx_t		*mlx;
 	mlx_image_t	*img;
 
-	mlx = mlx_init(800, 800, "Fractol", true);
-	if (!mlx)
+	stc.mlx = mlx_init(WIDTH, HEIGHT, "Fractol", true);
+	if (!stc.mlx)
 		exit(1);
-	img = mlx_new_image(mlx, 800, 800);
-	if (!img)
+	stc.img = mlx_new_image(stc.mlx, WIDTH, HEIGHT);
+	if (!stc.img)
 	{
-		mlx_close_window(mlx);
-		free(mlx);
+		mlx_close_window(stc.mlx);
+		free(stc.mlx);
 		exit(1);
 	}
 
-	for (int r = 0; r < 800; r++)
+	for (int r = 0; r < HEIGHT; r++)
 	{
-		for (int c = 0; c < 800; c++)
+		for (int c = 0; c < WIDTH; c++)
 		{
-			ft_in_mandelbrot(img, r, c);
+			ft_in_mandelbrot(&stc, r, c);
 		}
 	}
-	mlx_image_to_window(mlx, img, 0, 0);
-	mlx_loop(mlx);
+	mlx_image_to_window(stc.mlx, stc.img, 0, 0);
+	mlx_loop(stc.mlx);
 	return (0);
 }
 
